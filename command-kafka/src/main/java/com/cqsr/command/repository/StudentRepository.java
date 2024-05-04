8ab8e8d35b65bd0d79875865c97420795e89893a
diallo <mamadouadama609@gmail.com>
2024-05-04 11:37:39 -0500

    implemented queries

---
+++ command-kafka/src/main/java/com/cqsr/command/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package com.cqsr.command.repository;
+
+
+import com.cqsr.command.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long> {
+}
