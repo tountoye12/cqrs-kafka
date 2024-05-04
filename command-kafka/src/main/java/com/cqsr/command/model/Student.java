269cd7a85fe9e37a6be460c5a47e1fbb5f37b54f
Ali Bouzar <ali.bouzar@gmail.com>
2024-05-04 10:42:02 -0500

    Created Controller and Service layer for Command service

---
+++ command-kafka/src/main/java/com/cqsr/command/model/Student.java
@@ -0,0 +1,26 @@
+package com.cqsr.command.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Builder
+public class Student {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	private String firstname;
+	private String lastname;
+	private String course;
+	private double grade;
+}
